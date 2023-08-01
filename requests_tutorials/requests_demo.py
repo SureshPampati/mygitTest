3f700b76431d759df8b083453ca8fc700c89d5d3
Suresh Pampati <psuresh9751@gmail.com>
2023-08-01 16:09:20 +0530

    test1

--- requests_tutorials/requests_demo.py
+++ requests_tutorials/requests_demo.py
@@ -1,5 +1,34 @@
 import requests
+url = 'https://jsonplaceholder.typicode.com/posts/'
 
-response = requests.get('https://w3schools.com/python/demopage.htm')
+# adding a payload
+payload = {'id':[1,2], 'userId':1}
+
+# get requests allows to retrieve data
+response = requests.get(url,params=payload)
+response1 = requests.head(url)
 print(response.text)
-print("hi")
+
+print(response.json())
+
+print(response.status_code)
+
+
+
+# post allows to create new data
+
+# define new data to create
+
+new_data = {
+    "userID": 1,
+    "id": 1,
+    "title": "Making a POST request",
+    "body": "This is the data we created"
+}
+
+url_post = "https://jsonplaceholder.typicode.com/posts"
+
+post_response = requests.post(url_post, new_data)
+
+print(post_response.json())
+
